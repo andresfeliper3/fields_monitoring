05b213f88cd058b37de1f56ae3ac698d86a43265
andresfeliper3 <andresfeliper.2001@gmail.com>
2024-06-07 10:09:20 -0500

    logger config and logs in console added

---
+++ config/logging_config.py
@@ -0,0 +1,16 @@
+# logging_config.py
+import logging
+import sys
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.StreamHandler(sys.stdout),
+            logging.FileHandler("app.log")
+        ]
+    )
+
+# Call setup_logging() to configure logging when the module is imported
+setup_logging()
