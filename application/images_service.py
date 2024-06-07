56664bd4e3fe90d7ec14b06647acef70c9028aeb
andresfeliper3 <andresfeliper.2001@gmail.com>
2024-06-07 11:48:06 -0500

    typing added to service

--- application/images_service.py
+++ application/images_service.py
@@ -1,8 +1,7 @@
-# images_service.py
 import logging
 import io
 import zipfile
-from typing import List, Optional
+from typing import List, Optional, Union
 from fastapi import Depends
 from domain.repositories.images_repository import ImagesRepository
 from infrastructure.repositories.images_repository_impl import ImagesRepositoryImpl
@@ -11,13 +10,13 @@ logger = logging.getLogger(__name__)
 
 class ImagesService:
     def __init__(self, images_repository: ImagesRepository = Depends(ImagesRepositoryImpl)) -> None:
-        self.images_repository = images_repository
+        self.images_repository: ImagesRepository = images_repository
 
     async def load_images_from_nasa_api(self) -> bool:
-        fields = self.images_repository.process_fields()
+        fields: Optional[List[Field]] = self.images_repository.process_fields()
         if fields:
-            images = await self.images_repository.get_images_from_nasa_api(fields)
-            upload_status = self.images_repository.upload_to_s3(images)
+            images: List[dict] = await self.images_repository.get_images_from_nasa_api(fields)
+            upload_status: bool = self.images_repository.upload_to_s3(images)
             logger.info("Images processed and uploaded to S3")
             return upload_status
         else:
@@ -28,22 +27,22 @@ class ImagesService:
         return self.images_repository.list_images_in_s3()
 
     def download_images_as_zip(self) -> Optional[io.BytesIO]:
-        images = self.images_repository.list_images_in_s3()
+        images: Optional[List[str]] = self.images_repository.list_images_in_s3()
         if not images:
             logger.warning("No images available for download")
             return None
         return self._create_zip_from_images(image_keys=images)
 
     def download_images_by_field_id_as_zip(self, field_id: str) -> Optional[io.BytesIO]:
-        images = self.images_repository.get_images_by_field_id(field_id)
+        images: Optional[List[str]] = self.images_repository.get_images_by_field_id(field_id)
         if not images:
             logger.warning(f"No images available for field ID {field_id}")
             return None
         return self._create_zip_from_images(image_keys=images)
 
     def _create_zip_from_images(self, image_keys: List[str]) -> io.BytesIO:
-        images = self.images_repository.download_images_from_s3(image_keys)
-        zip_buffer = io.BytesIO()
+        images: List[dict] = self.images_repository.download_images_from_s3(image_keys)
+        zip_buffer: io.BytesIO = io.BytesIO()
         with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
             for image in images:
                 zip_file.writestr(image['key'], image['content'])
